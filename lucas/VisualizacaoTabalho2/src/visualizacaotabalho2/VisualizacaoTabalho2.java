eb1b82150362dc029a015eb89437beeca009a731
Lucas Scheunemann <l182441@g.unicamp.br>
2019-05-22 11:59:53 -0300

    atualização com adesão de total de palavras

--- lucas/VisualizacaoTabalho2/src/visualizacaotabalho2/VisualizacaoTabalho2.java
+++ lucas/VisualizacaoTabalho2/src/visualizacaotabalho2/VisualizacaoTabalho2.java
@@ -6,10 +6,17 @@
 package visualizacaotabalho2;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -23,6 +30,7 @@ public class VisualizacaoTabalho2 {
 public static final int INICIAL = 1;
     /**
      * @param args the command line arguments
+     * @throws java.io.FileNotFoundException
      */
     public static void main(String[] args) throws FileNotFoundException, IOException {
         
@@ -35,37 +43,52 @@ public static final int INICIAL = 1;
         for (File diretorio1 : diretorio) { //para cada arquivo no diretorio
             
             System.out.println(diretorio1.toString()); //mostra o caminho do arquivo sendo lido atualmente
-            FileReader FiRe = new FileReader(diretorio1); //lê cada arquivo
-            Scanner scan = new Scanner(FiRe); //leitor de arquivos
+            
+            
+            Scanner scan = new Scanner(new FileInputStream(diretorio1), "latin1"); //leitor de arquivos
             
             
             while (scan.hasNext()) { //enquanto tiver texto,
-                String[] linha = scan.nextLine().split("\\s"); //salva cada palavra numa String
+                String[] linha = scan.nextLine().split("\\s"); //separa as palavras, salva cada palavra numa String
                 
-                System.out.println(Arrays.toString(linha) + linha.length); //e imprime na tela o conjunto de palavras da linha.
                 
                 for (String linha1 : linha) {//para cada palavra da linha
+                    
+                    //-- filtro das palavras --//
                     
                     linha1 = linha1.replace(".", "");//retirada de "."
                     linha1 = linha1.replace(",", "");//retirada de ","
                     linha1 = linha1.replace("\"", "");//retirada de """
-                                        
+                    
+                    if (BaseDeFrequencia.STOPWORDS.contains(linha1)){//se estiver na base de stopwords,
+                        continue; //passa pra próxima
+                    }
+                    // ---------------------//
+
+                    
                     if (base.getMap().isEmpty()) { //se a base estiver vazia,
                         base.getMap().put(linha1, INICIAL); //adiciona a palavra na base e adiciona 1 na contagem da mesma.
+                        
+                        BaseDeFrequencia.total = BaseDeFrequencia.total + INICIAL; //contagem do número total de palavras
+                
                     }
                     else {//se não estiver vazia,
                         if (base.getMap().containsKey(linha1)){ //se a palavra está na base,
                             base.getMap().put(linha1, base.getMap().get(linha1)+1);//adiciona 1 na contagem dele.
-                                                       
+                            BaseDeFrequencia.total = BaseDeFrequencia.total + INICIAL; //contagem do número total de palavras
+                                           
                         }
                         else { //se não estiver na base,
                             base.getMap().put(linha1, INICIAL);//adiciona na próxima posição e conta a primeira aparição.
-                            
+                            BaseDeFrequencia.total = BaseDeFrequencia.total + INICIAL; //contagem do número total de palavras
+                
                         }
                     }
                 }
-                System.out.println(base.getMap().toString() + " \n ");
+                
             }
+            System.out.println(base.getMap().toString() + " \n total: " + BaseDeFrequencia.total); //apresentação da base de frequencia e total de palavras
+
         }
         
         
